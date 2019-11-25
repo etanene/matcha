328aea6d955a064ad97bb7dc040326d79a1e55d2
Arnbjorn Falmer archer <afalmer-@ox-b5.21-school.ru>
2019-11-25 16:46:45 +0300

    reg from layout

--- frontend/src/Components/Input/Input.jsx
+++ frontend/src/Components/Input/Input.jsx
@@ -7,22 +7,40 @@ import './Input.css';
 const inputCss = cn('input');
 
 function Input(props) {
-  const { size } = props;
+  const {
+    size,
+    type,
+    placeholder,
+    children,
+    cls,
+  } = props;
 
   return (
-    <div className={inputCss()}>
-      <input type="text" className={inputCss('input-tag', { size })} />
-      <span>hello</span>
+    <div className={inputCss({}, [cls])}>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={inputCss('input-tag', { size })}
+      />
+      { children && <span>{children}</span> }
     </div>
   );
 }
 
 Input.propTypes = {
   size: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  children: PropTypes.string,
+  cls: PropTypes.string,
 };
 
 Input.defaultProps = {
   size: 's',
+  type: 'text',
+  placeholder: null,
+  children: null,
+  cls: null,
 };
 
 export default Input;
--- frontend/src/Components/RegForm/RegForm.jsx
+++ frontend/src/Components/RegForm/RegForm.jsx
@@ -3,17 +3,61 @@ import PropTypes from 'prop-types';
 import { cn } from '@bem-react/classname';
 
 import Input from '../Input/Input';
+import Button from '../Button/Button';
 import './RegForm.css';
 
 const regFormCss = cn('reg-form');
+const inputCss = regFormCss('input');
 
 function RegForm(props) {
   const { cls } = props;
 
   return (
     <div className={regFormCss({}, [cls])}>
-      <span>Sign Up</span>
-      <Input />
+      <span className={regFormCss('form-name')}>Sign Up</span>
+      <Input
+        type="text"
+        placeholder="Username"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Input
+        type="text"
+        placeholder="Email"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Input
+        type="text"
+        placeholder="First name"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Input
+        type="text"
+        placeholder="Last name"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Input
+        type="text"
+        placeholder="Password"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Input
+        type="text"
+        placeholder="Confirm password"
+        cls={inputCss}
+      >
+        validation
+      </Input>
+      <Button>Sign up</Button>
     </div>
   );
 }
