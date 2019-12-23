a9d5e07e9abd0b1fdcff208ab2b8a18498ab571b
Александр Коломиец <aleksandr@MacBook-Air-Aleksandr.local>
2019-12-23 10:28:18 +0300

    handle change input file

--- frontend/src/Components/PhotoInput/PhotoInput.jsx
+++ frontend/src/Components/PhotoInput/PhotoInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { cn } from '@bem-react/classname';
 
@@ -10,17 +10,37 @@ const photoInputCss = cn('photo-input');
 
 function PhotoInput(props) {
   const { id, cls } = props;
+  const [file, setFile] = useState();
+
+  function handleChange(event) {
+    const reader = new FileReader();
+    const inputFile = event.target.files[0];
+
+    reader.onloadend = () => {
+      setFile(reader.result);
+    };
+    reader.readAsDataURL(inputFile);
+  }
+
+  console.log(file);
+  const content = file ? (
+    <div className={photoInputCss('photo')}>
+      <img src={file} alt="img" className={photoInputCss('img')} />
+    </div>
+  ) : (
+    <div className={photoInputCss('card')}>
+      <Icon fill="grey" size="l" viewBox="0 0 315 315" icon={ICONS.ADD} />
+      <span>Add photo</span>
+      <input type="file" onChange={handleChange} id={photoInputCss(`input${id}`)} className={photoInputCss('input')} />
+    </div>
+  );
 
   return (
     <div className={photoInputCss({}, [cls])}>
       <label htmlFor={photoInputCss(`input${id}`)}>
         <div className={photoInputCss('layer')}>
-          <div className={photoInputCss('card')}>
-            <Icon fill="grey" size="l" viewBox="0 0 315 315" icon={ICONS.ADD} />
-            <span>Add photo</span>
-          </div>
+          {content}
         </div>
-        <input type="file" id={photoInputCss(`input${id}`)} />
       </label>
     </div>
   );
