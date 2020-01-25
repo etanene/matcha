ed7a7f1ea64dbdae2bde9c64a16c3daa77c52b90
Arnbjorn Falmer archer <afalmer-@il-j5.21-school.ru>
2020-01-25 16:07:52 +0300

    add modal to common components

--- frontend/src/Components/LoadingModal/LoadingModal.jsx
+++ frontend/src/Components/LoadingModal/LoadingModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Modal from '../Modal/Modal';
+import Modal from '../common/Modal/Modal';
 import Spinner from '../common/Spinner/Spinner';
 
 function LoadingModal(props) {
