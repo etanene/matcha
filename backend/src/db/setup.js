e46114bd39f47f19fa4bd43202fb3a6a298c5943
Norma Tothmure <ntothmur@at-k2.21-school.ru>
2020-02-23 17:57:29 +0300

    merge

--- backend/src/db/setup.js
+++ backend/src/db/setup.js
@@ -10,6 +10,10 @@ const root = fs.readFileSync(path.join(__dirname, '/migrates/root.sql')).toStrin
 const photos = fs.readFileSync(path.join(__dirname, '/migrates/photos.sql')).toString();
 const tags = fs.readFileSync(path.join(__dirname, '/migrates/tags.sql')).toString();
 const taggings = fs.readFileSync(path.join(__dirname, '/migrates/taggings.sql')).toString();
+const fakeusers = fs.readFileSync(path.join(__dirname, '/migrates/fakeusers.sql')).toString();
+const faketags = fs.readFileSync(path.join(__dirname, '/migrates/faketags.sql')).toString();
+const faketaggings = fs.readFileSync(path.join(__dirname, '/migrates/faketaggings.sql')).toString();
+const fakephotos = fs.readFileSync(path.join(__dirname, '/migrates/fakephotos.sql')).toString();
 
 (async () => {
   await client.connect();
@@ -33,6 +37,18 @@ const taggings = fs.readFileSync(path.join(__dirname, '/migrates/taggings.sql'))
   // Создаем таблицу taggings
   await client.query(taggings);
   console.log('Table taggings created');
+
+  await client.query(fakeusers);
+  console.log('Fake users created');
+
+  await client.query(faketags);
+  console.log('Fake tags created');
+
+  await client.query(faketaggings);
+  console.log('Fake taggings created');
+
+  await client.query(fakephotos);
+  console.log('Fake photos created');
 
   client.end();
 })();
