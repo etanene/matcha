60e02ba8cbd684143cd0fb46b3bf00dbfcda78f9
Norma Tothmure <ntothmur@at-l8.21-school.ru>
2020-02-28 18:10:02 +0300

    add socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const router = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use('/public', express.static(path.resolve(__dirname, '../public')));
 app.use(cors());
@@ -18,6 +22,16 @@ app.use(session({
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+io.on('connection', (socket) => {
+  console.log('We have a new connection!');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 app.use('/', router);
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- frontend/src/Components/Chat/Chat.jsx
+++ frontend/src/Components/Chat/Chat.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:8000');
 
 function Chat() {
+  console.log(socket);
+
   return (
     <div>Chat</div>
   );
