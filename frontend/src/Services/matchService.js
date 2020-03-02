1cb5f3e6e33aae1df820b8e3d99b8b1f73cc9079
Arnbjorn Falmer archer <afalmer-@at-l7.21-school.ru>
2020-03-02 22:31:13 +0300

    add math service

---
+++ frontend/src/Services/matchService.js
@@ -0,0 +1,21 @@
+let socket;
+
+const connect = () => {
+  socket = new WebSocket('ws://localhost:8000');
+
+  socket.onopen = (e) => {
+    console.log('connection opened');
+    console.log('open event', e);
+  };
+  socket.onclose = (e) => {
+    console.log('connection closed');
+    console.log('close event', e);
+  };
+  socket.onerror = (err) => {
+    console.log('error socket', err);
+  };
+};
+
+export default {
+  connect,
+};
